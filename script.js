fa4419a9b1638a4874ab004d1539483a14817dfd
Joel Lee <leejoel010@gmail.com>
2021-07-29 21:41:21 +0100

    Complete Etch-A-Sketch funcionality within script.js, including random RGB and gradual darkness options

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ const mainContainer = document.querySelector("#main-container");
 function createSquares(number) {
     for (let i = 1; i <= number ** 2; i++) {
         const playSquare = document.createElement("div");
+        let startingBlack = 100;
+
         playSquare.className = "play-div";
+        playSquare.style.backgroundColor = "hsl(0, 0%, 100%)";
         playSquare.addEventListener("mouseover", () => {
+            // Changes square to random RGB on rollover
+            /* playSquare.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`; */
+
+            // Decreases square lightness by 10% per rollover
+            /* startingBlack = decreaseHSL(startingBlack);
+            playSquare.style.backgroundColor = `hsl(0, 0%, ${startingBlack}%)`; */
+
             playSquare.style.backgroundColor = "black";
         });
 
@@ -15,17 +25,32 @@ function createSquares(number) {
     mainContainer.style.gridTemplateRows = `repeat(${number}, 1fr)`;
 }
 
-function removeGrid() {
-    let allSquares = document.querySelectorAll(".play-div");
+function randomRGB() {
+    let randomRGB = Math.trunc(Math.random() * 255);
+    return randomRGB;
+}
 
-    document.body.removeChild("#main-container");
+function decreaseHSL(input) {
+    if (input == 0) {
+        return input;
+    } else {
+        let newInput = input - 10;
+        return Number(newInput);
+    }
 }
 
 function resetGrid() {
     let allSquares = document.querySelectorAll(".play-div");
     let userInput = Number(prompt("How many squares on each side? (Max. 100)", ""));
 
-    removeGrid();
+    if (userInput <= 100) {
+    mainContainer.textContent = "";
+    createSquares(userInput);
+    } else if (userInput > 100) {
+        alert("I SAID LESS THAN 100.");
+    } else if (userInput < 0 || typeof userInput != Number) {
+        alert("WHAT? WHY.")
+    }
 }
 
 function addButton() {
@@ -33,9 +58,9 @@ function addButton() {
 
     resetButton.textContent = "Reset";
     resetButton.id = "reset-button";
-/*     resetButton.addEventListener("click", () => {
+    resetButton.addEventListener("click", () => {
         resetGrid();
-    }); */
+    });
 
     document.body.appendChild(resetButton);
 }
