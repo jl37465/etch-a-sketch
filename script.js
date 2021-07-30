4ffd3f8dc5b9dd9fec5e03c1b99ed79d082c9005
Joel Lee <leejoel010@gmail.com>
2021-07-30 21:52:21 +0100

    Added buttons for each variant and some design features, including rollover color changes

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
 const mainContainer = document.querySelector("#main-container");
 
-function createSquares(number) {
-    for (let i = 1; i <= number ** 2; i++) {
-        const playSquare = document.createElement("div");
-        let startingBlack = 100;
+function createSquares(number, style) {
+    if (style === "black") {
+        for (let i = 1; i <= number ** 2; i++) {
+            const playSquare = document.createElement("div");
+            let startingBlack = 100;
 
-        playSquare.className = "play-div";
-        playSquare.style.backgroundColor = "hsl(0, 0%, 100%)";
-        playSquare.addEventListener("mouseover", () => {
-            // Changes square to random RGB on rollover
-            /* playSquare.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`; */
+            playSquare.className = "play-div";
+            playSquare.style.backgroundColor = "hsl(0, 0%, 100%)";
+            playSquare.addEventListener("mouseover", () => {
+                // Changes square to random RGB on rollover
+                /* playSquare.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`; */
 
-            // Decreases square lightness by 10% per rollover
-            /* startingBlack = decreaseHSL(startingBlack);
-            playSquare.style.backgroundColor = `hsl(0, 0%, ${startingBlack}%)`; */
+                // Decreases square lightness by 10% per rollover
+                /* startingBlack = decreaseHSL(startingBlack);
+                playSquare.style.backgroundColor = `hsl(0, 0%, ${startingBlack}%)`; */
 
-            playSquare.style.backgroundColor = "black";
-        });
+                playSquare.style.backgroundColor = "black";
+            });
 
-        mainContainer.appendChild(playSquare);
+            mainContainer.appendChild(playSquare);
+        }
+
+    } else if (style ==="multicolor") {
+        for (let i = 1; i <= number ** 2; i++) {
+            const playSquare = document.createElement("div");
+            let startingBlack = 100;
+
+            playSquare.className = "play-div";
+            playSquare.style.backgroundColor = "hsl(0, 0%, 100%)";
+            playSquare.addEventListener("mouseover", () => {
+                // Changes square to random RGB on rollover
+                playSquare.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+            });
+
+            mainContainer.appendChild(playSquare);
+        }
+    } else {
+        for (let i = 1; i <= number ** 2; i++) {
+            const playSquare = document.createElement("div");
+            let startingBlack = 100;
+
+            playSquare.className = "play-div";
+            playSquare.style.backgroundColor = "hsl(0, 0%, 100%)";
+            playSquare.addEventListener("mouseover", () => {
+                // Decreases square lightness by 10% per rollover
+                startingBlack = decreaseHSL(startingBlack);
+                playSquare.style.backgroundColor = `hsl(0, 0%, ${startingBlack}%)`;
+
+            });
+
+            mainContainer.appendChild(playSquare);
+        }
     }
 
     mainContainer.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
@@ -39,34 +72,71 @@ function decreaseHSL(input) {
     }
 }
 
-function resetGrid() {
+function resetGrid(style) {
     let allSquares = document.querySelectorAll(".play-div");
-    let userInput = Number(prompt("How many squares on each side? (Max. 100)", ""));
+    let userInput = prompt("How many squares on each side? (Max. 100)", "");
+    let userNumber = Number(userInput);
 
-    if (userInput <= 100) {
-    mainContainer.textContent = "";
-    createSquares(userInput);
-    } else if (userInput > 100) {
+    if (userNumber <= 100) {
+        mainContainer.textContent = "";
+        createSquares(userNumber, style);
+    } else if (userInput === "") {
+        mainContainer.textContent = "";
+        createSquares(16, style);
+    } else if (userNumber > 100) {
         alert("I SAID LESS THAN 100.");
-    } else if (userInput < 0 || typeof userInput != Number) {
+    } else if (userNumber < 0) {
         alert("WHAT? WHY.")
-    }
+    } 
 }
 
-function addButton() {
-    const resetButton = document.createElement("button");
+function addButtonFunctions() {
+    const resetButton = document.querySelector("#reset-button");
+    const multicolorButton = document.querySelector("#multicolor-button");
+    const monochromeButton = document.querySelector("#monochrome-button");
+    const buttonsDiv = document.querySelector("#buttons-div");
 
-    resetButton.textContent = "Reset";
-    resetButton.id = "reset-button";
     resetButton.addEventListener("click", () => {
-        resetGrid();
+        resetGrid("black");
+    });
+    resetButton.addEventListener("mouseover", () => {
+        resetButton.style.backgroundColor = `black`;
+        resetButton.style.color = "white";
+        resetButton.style.border = "solid 5px white";
+    });
+    resetButton.addEventListener("mouseout", () => {
+        resetButton.style.backgroundColor = "white";
+        resetButton.style.color = "black";
+        resetButton.style.border = "solid 5px black";
     });
 
-    document.body.appendChild(resetButton);
+    multicolorButton.addEventListener("click", () => {
+        resetGrid("multicolor");
+    });
+
+    // Changes button background color to random RGB upon hover, then reverts when hover stops.
+    multicolorButton.addEventListener("mouseover", () => {
+        multicolorButton.style.backgroundColor = `rgb(${randomRGB()}, ${randomRGB()}, ${randomRGB()})`;
+    });
+    multicolorButton.addEventListener("mouseout", () => {
+        multicolorButton.style.backgroundColor = "white";
+    });
+    
+    monochromeButton.addEventListener("click", () => {
+        resetGrid("shade");
+    });
+
+    // Reverses color scheme upon hover, then reverts when hover stops.
+    monochromeButton.addEventListener("mouseover", () => {
+        monochromeButton.style.background = "linear-gradient(90deg,white 50%, black 50%, black)";
+    });
+    monochromeButton.addEventListener("mouseout", () => {
+        monochromeButton.style.background = "linear-gradient(90deg,black 50%, white 50%, white)";
+    });
 }
 
-addButton();
-createSquares(16);
+addButtonFunctions();
+createSquares(16, "black");
 
 // TO DO: MAKE BUTTON PROMPT USER FOR NUMBER, WHICH IS USED FOR GRID.
 // TO DO: ADD COMMENTS AND STUFF.
